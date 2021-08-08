411ef65cfcd62e186919c3f512eba1e43b8a658d
camiloporto <camiloporto@gmail.com>
2021-08-08 15:56:27 +1000

    adding account Statement Service

--- src/app/account-statement/account-statement.component.ts
+++ src/app/account-statement/account-statement.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import {FormGroup, FormControl} from '@angular/forms';
+import { MatDatepickerInputEvent, MatDatepickerIntl } from '@angular/material/datepicker';
+// import { AccountStatementService } from './account-statement.service';
 
 export interface AccountStatementItemUI {
   date: Date;
@@ -17,26 +19,6 @@ const SAMPLE_DATA: AccountStatementItemUI[] = [
   {date: new Date("2019-01-07"), account: 'Internet', description: 'pagamento myReuplic', amount: -64, tags: ['aluguel']},
 ]
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
-
 @Component({
   selector: 'app-account-statement',
   templateUrl: './account-statement.component.html',
@@ -52,10 +34,32 @@ export class AccountStatementComponent implements OnInit {
     end: new FormControl()
   });
 
-  constructor() { }
+  constructor() {
+
+   }
 
   ngOnInit() {
   }
+
+  dateSelected(event: MatDatepickerInputEvent<Date>) {
+    if(this.isValidDateRange()) {
+      this.fetchTableData()
+      console.log(' date range is valid. appply filter');
+    }
+    console.log(' date range is not valid. Do nothing');
+  }
+
+  fetchTableData() {
+    // this.accountStatementService.getAccountStatement(
+    //   'someAID', 
+    //   this.getReportStartDate(), 
+    //   this.getReportEndDate());
+  }
+
+  isValidDateRange() : boolean {
+    return (this.getReportStartDate() != null && this.getReportEndDate() != null);
+  }
+
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
---
+++ src/app/account-statement/account-statement.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+
+export class AccountStatementItem {
+  date: Date;
+  account: string;
+  description: string;
+  amount: number;
+  tags: string[];
+}
+
+export class AccountStatement {
+  begin: Date;
+  end: Date;
+  accountId: string;
+  entries: AccountStatementItem[];
+}
+
+@Injectable()
+export class AccountStatementService {
+
+  constructor() { }
+
+  getAccountStatement(accountId: string, begin: Date, end: Date) : AccountStatement {
+    return new AccountStatement();
+  }
+
+}
+
